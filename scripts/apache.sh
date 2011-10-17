069ec1521b2c3f5c2c2966cb72b4a8b9a037f74b
Joris Berthelot <admin+github@eexit.net>
2011-10-17 08:57:40 +0000

    updated apache script + conf + added global setup script

--- scripts/apache.sh
+++ scripts/apache.sh
@@ -1,15 +1,34 @@
 !#/bin/sh
 
-CONF_DIR=`pwd`"/../confs"
-APACHE_CONF=$CONF_DIR
-SERVER_NAME="www.g1b5.tp.org"
-SERVER_ROOT=/var/org/tp/g1b5
+APP_NAME=g1b5
+CONF_DIR=`pwd`/../confs
+APACHE_CONF=$CONF_DIR/apache.conf
+SERVER_NAME=www.$APP_NAME.tp.org
+SERVER_ROOT=/var/org/tp/$APP_NAME
+
+if [ "`whoami`" != 'root' ]; then
+    echo "You have to be root user to run $0!"
+    exit 1;
+fi
 
 clear
+
+echo
+echo
 echo "==========================="
 echo "= Apache Installer Script ="
 echo "==========================="
+echo
+echo
+
 /etc/init.d/httpd stop
 
-echo "::1   $SERVER_NAME" >> /etc/hosts
+echo "127.0.0.1     $SERVER_NAME" >> /etc/hosts
+echo "::1           $SERVER_NAME" >> /etc/hosts
+
+if [ -d $SERVER_ROOT ]; then
+    mkdir -p $SERVER_ROOT
+fi
+
 touch $SERVER_ROOT/ha_state
+cp $APACHE_CONF /etc/httpd/conf.d/$APP_NAME.conf
