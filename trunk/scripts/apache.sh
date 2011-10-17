01d6aef0707697cf8f226f0110c6026115e82557
joris.berthelot@gmail.com <joris.berthelot@gmail.com@663ef947-ee91-8a61-d4fd-d8c2e4fbfccd>
2011-10-17 14:03:35 +0000

    updated install script for apache

--- trunk/scripts/apache.sh
+++ trunk/scripts/apache.sh
@@ -1,7 +1,8 @@
 !#/bin/sh
 
-CONF_DIR=`pwd`/../confs
-APACHE_CONF=$CONF_DIR/apache.conf
+HERE=`pwd`
+CONF_DIR=$HERE/../confs/apache
+APACHE_CONF=$CONF_DIR/g1b5.conf
 SERVER_NAME=www.g1b5.tp.org
 SERVER_ROOT=/var/www/org/tp/g1b5/web
 
@@ -28,16 +29,61 @@ echo
 #echo "::1           $SERVER_NAME" >> /etc/hosts
 
 if [ ! -d $SERVER_ROOT ]; then
-    mkdir -p $SERVER_ROOT
+	echo
+	echo Creating Web root directory...
+    mkdir -p --verbose $SERVER_ROOT
+	chown -R apache:apache $SERVER_ROOT
+	echo
+	echo [ OK ]
 fi
 
 if [ ! -e $SERVER_ROOT/ha_state ]; then
+	echo
+	echo Creating Heartbeat log file
     touch $SERVER_ROOT/ha_state
+	echo [ OK ]
 fi
 
-if [(! -e /etc/httpd/conf.d/g1b5.conf) -o ("`md5sum $APACHE_CONF`" != "`md5sum /etc/httpd/conf.d/g1b5.conf`")]; then
-    cp $APACHE_CONF /etc/httpd/conf.d/g1b5.conf
+if [ -e /etc/httpd/conf.d/g1b5.conf ]; then
+	cd /etc/httpd/conf.d
+	LOCAL_SUM=`md5sum g1b5.conf`
+	cd $HERE
+	cd $CONF_DIR
+	
+	echo
+	echo Apache conf file already exists...
+	
+	if [ "$LOCAL_SUM" != "`md5sum g1b5.conf`" ]; then
+		echo ...but outdated... Updating Apache conf file
+		echo
+		cp -v $APACHE_CONF /etc/httpd/conf.d/g1b5.conf
+		echo
+		echo [ OK ]
+	fi
+	cd $HERE
+	echo ...and up to date!
+elif [ ! -e /etc/httpd/conf.d/g1b5.conf ]; then
+	echo
+	echo Creating Apache conf file...
+	echo
+    cp -v $APACHE_CONF /etc/httpd/conf.d/g1b5.conf
+	echo
+	echo [ OK ]
 fi
+
+echo
+echo Staging WebApplication into Web root directory...
+echo
+cp -rf $HERE/../lib/webserver /tmp/web
+#find /tmp/web -name ".svn" -type d -exec rm -rf {} \; -nowarn
+rm -rf $SERVER_ROOT/*
+mv -v /tmp/web/* $SERVER_ROOT
+echo
+echo [ OK ]
+echo
 
 /etc/init.d/httpd start
+
+echo
+echo
 
