fc3cc6fd2152a91ee33cd6c6e599a20e48e3639e
Joris Berthelot <admin+github@eexit.net>
2011-10-20 20:26:31 +0000

    Script packing RC1

--- scripts/bind.sh
+++ scripts/bind.sh
@@ -8,44 +8,47 @@ txtrst=$(tput sgr0)             # Reset
 info=${bldwht}*${txtrst}        # Feedback
 pass=${bldblu}*${txtrst}
 warn=${bldred}!${txtrst}
-
-CONF_DIR=`pwd`/../confs/bind
-MASTER_CONF=$CONF_DIR/named.conf
-SLAVE_CONF=$CONF_DIR/named.conf.slave
-ZONE_FILE=g1b5.tp.org.zone
-RV4_ZONE_FILE=rv4.g1b5.tp.org.zone
-RV6_ZONE_FILE=rv6.g1b5.tp.org.zone
-
 if [ "`whoami`" != "root" ]; then
     echo
-    echo -e "${warn}${warn}${warn} \aYou must be root to run $0! Go away nqqb!"
+    echo -e "${warn}${warn}${warn} \aYou must be root to run $0! Go rtfm nqqb!"
     echo
     exit 1;
 fi
-
-clear
-
+CONF_DIR=`pwd`/../confs/bind
+MASTER_CONF=$CONF_DIR/named.conf
+SLAVE_CONF=$CONF_DIR/named.conf.slave
+ZONE_FILE="g1b5.tp.org.zone"
+RV4_ZONE_FILE="rv4.g1b5.tp.org.zone"
+RV6_ZONE_FILE="rv6.g1b5.tp.org.zone"
 echo
 echo
 echo "========================="
-echo "= BIND Installer Script ="
+echo "= Bind Installer Script ="
 echo "========================="
 echo
 echo
-
+echo "Script developped by Joris Berthelot and Laurent Le Moine Copyright (c) 2011"
+echo
+echo
 /etc/init.d/named stop
-
-read -p "Are you the DNS master? (y/n) " -n 1 ANS
+read -p "Are you the primary name server? (y/n) " -n 1 ANS
+echo
 if [ "y" == $ANS ]; then
-	cp -f $MASTER_CONF /etc/named.conf
-	cp -f $CONF_DIR/$ZONE_FILE /etc/named/$ZONE_FILE
-	cp -f $CONF_DIR/$RV4_ZONE_FILE /etc/named/$RV4_ZONE_FILE
-	cp -f $CONF_DIR/$RV6_ZONE_FILE /etc/named/$RV6_ZONE_FILE
+    echo "Staging primary configuration files into destroot..."
+    echo
+	cp -f -v $MASTER_CONF /etc/named.conf
+	cp -f -v $CONF_DIR/$ZONE_FILE /etc/named/$ZONE_FILE
+	cp -f -v $CONF_DIR/$RV4_ZONE_FILE /etc/named/$RV4_ZONE_FILE
+	cp -f -v $CONF_DIR/$RV6_ZONE_FILE /etc/named/$RV6_ZONE_FILE
+    echo "[   ${bldblu}OK${txtrst}   ]"
 else
-	cp -f $SLAVE_CONF /etc/named.conf
+    echo "Staging slave configuration files into destroot..."
+    echo
+	cp -f -v $SLAVE_CONF /etc/named.conf
+    echo "[   ${bldblu}OK${txtrst}   ]"
 fi
-
 echo
 /etc/init.d/named start
-
-
+echo
+echo "[   ${bldblu}DONE${txtrst}   ]"
+echo
--- scripts/ldap.sh
+++ scripts/ldap.sh
@@ -8,21 +8,16 @@ txtrst=$(tput sgr0)             # Reset
 info=${bldwht}*${txtrst}        # Feedback
 pass=${bldblu}*${txtrst}
 warn=${bldred}!${txtrst}
-
+if [ "`whoami`" != "root" ]; then
+    echo
+    echo -e "${warn}${warn}${warn} \aYou must be root to run $0! Go rtfm nqqb!"
+    echo
+    exit 1;
+fi
 CONF_DIR=`pwd`/../confs/ldap
 LDAP_CONF=$CONF_DIR/ldap.conf
 SLAPD_CONF=$CONF_DIR/slapd.conf
 LDIF_FILE=$CONF_DIR/g1b5.ldif
-
-if [ "`whoami`" != "root" ]; then
-    echo
-    echo -e "${warn}${warn}${warn} \aYou must be root to run $0! Go away nqqb!"
-    echo
-    exit 1;
-fi
-
-clear
-
 echo
 echo
 echo "========================="
@@ -30,25 +25,33 @@ echo "= LDAP Installer Script ="
 echo "========================="
 echo
 echo
-
+echo "Script developped by Joris Berthelot and Laurent Le Moine Copyright (c) 2011"
+echo
+echo
 /etc/init.d/slapd stop
-
+echo "Configuring slapd...."
+echo
 rm -Rf /etc/openldap/slapd.d
+cp -v -f $LDAP_CONF /etc/openldap/ldap.conf
+cp -v -f $SLAPD_CONF /etc/openldap/slapd.conf
 
-cp $LDAP_CONF /etc/openldap/ldap.conf
-
-cp $SLAPD_CONF /etc/openldap/slapd.conf
-
-mkdir /var/cluster/ldap
-
+if [ ! -d /var/cluster/ldap ]; then
+    mkdir -p --verbose /var/cluster/ldap
+fi
 chown ldap:ldap /var/cluster/ldap
-
+echo
+echo "[   ${bldblu}OK${txtrst}   ]"
+echo
 /etc/init.d/slapd start
-
-read -p "Fill the database? (y/n) " -n 1 ANS
+echo
+echo
+read -p "Are you the primary node cluster (data reference)? (y/n) " -n 1 ANS
 if [ "y" == $ANS ]; then
 	echo
 	ldapadd -x -D 'cn=Manager,dc=g1b5,dc=tp,dc=org' -W -f $LDIF_FILE
+    echo "[   ${bldblu}OK${txtrst}   ]"
+    echo
 fi
 echo
-
+echo "[   ${bldblu}DONE${txtrst}   ]"
+echo
