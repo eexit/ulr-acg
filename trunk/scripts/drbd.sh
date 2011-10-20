ab0be308561dfcb2036eab9d0c39b28c177d3ab6
joris.berthelot@gmail.com <joris.berthelot@gmail.com@663ef947-ee91-8a61-d4fd-d8c2e4fbfccd>
2011-10-20 00:14:52 +0000

    pacemaker, drbd and apache update

---
+++ trunk/scripts/drbd.sh
@@ -0,0 +1,81 @@
+!#/bin/sh
+# Text color variables
+txtbld=$(tput bold)             # Bold
+bldred=${txtbld}$(tput setaf 1) #  red
+bldblu=${txtbld}$(tput setaf 4) #  blue
+bldwht=${txtbld}$(tput setaf 7) #  white
+txtrst=$(tput sgr0)             # Reset
+info=${bldwht}*${txtrst}        # Feedback
+pass=${bldblu}*${txtrst}
+warn=${bldred}!${txtrst}
+
+if [ "`whoami`" != "root" ]; then
+    echo
+    echo -e "\a${warn}You must be root to run $0! Go away nqqb!"
+    exit 1;
+fi
+
+HERE=`pwd`
+CONF_DIR=$HERE/../confs/drbd
+DRBD_CONF=$CONF_DIR/drbd.conf
+
+clear
+
+echo
+echo
+echo "========================="
+echo "= DRBD Installer Script ="
+echo "========================="
+echo
+echo
+echo "Script developped by Joris Berthelot and Laurent Le Moine Copyright (c) 2011"
+echo
+echo
+
+if [ ! -d /etc/drbd ]; then
+    echo -e "\a${warn}[ERROR] ${txtrst}drbd package not installed! Run setup.sh firstly..."
+    echo
+    exit 1;
+fi
+
+echo "Creating clone partition..."
+echo
+lvcreate -n drbd -L 1G ulr-acg
+echo
+echo "${pass}[ OK ]"
+
+echo "Configuring drbd..."
+cp -f $DRBD_CONF /etc/drbd
+echo
+echo "${pass}[ OK ]"
+echo
+
+echo "Building drbd resource..."
+echo
+drbdadm --force create-md ulracg
+modprobe drbd
+drbdadm up ulracg
+echo
+
+read -p "Clone cluster data from this node? (y/n)" -n 1 ANS
+if [ "y" == $ANS ]; then
+    drbdadm primary --force ulracg
+fi
+mkfs.ext4 /dev/drbd1
+echo
+
+echo "Moving /var content to the new partition..."
+cd /var
+cp -ax * /dev/drbd1
+cd /
+mv var var.old
+mkdir var
+mount /dev/drbd1 /var
+echo
+echo "${warn}Please, edit your /etc/fstab and /var to /dev/drbd1"
+echo
+read -p "Press any key to edit the /etc/fstab file..."
+vim /etc/fstab
+echo
+echo
+
--- trunk/scripts/pcmk.sh
+++ trunk/scripts/pcmk.sh
@@ -1,11 +1,21 @@
 !#/bin/sh
+# Text color variables
+txtbld=$(tput bold)             # Bold
+bldred=${txtbld}$(tput setaf 1) #  red
+bldblu=${txtbld}$(tput setaf 4) #  blue
+bldwht=${txtbld}$(tput setaf 7) #  white
+txtrst=$(tput sgr0)             # Reset
+info=${bldwht}*${txtrst}        # Feedback
+pass=${bldblu}*${txtrst}
+warn=${bldred}!${txtrst}
 
 if [ "`whoami`" != "root" ]; then
     echo
-    echo "You have to be root user to run $0!"
-    echo
+    echo -e "\a${warn}You must be root to run $0! Go away nqqb!"
     exit 1;
 fi
+
+COROSYNC_CONF="/etc/corosync/corosync.conf"
 
 clear
 
@@ -16,20 +26,22 @@ echo "= Pacemaker Installer Script ="
 echo "=============================="
 echo
 echo
+echo "Script developped by Joris Berthelot and Laurent Le Moine Copyright (c) 2011"
+echo
+echo
 
 if [ ! -d /etc/corosync ]; then
-    echo "[ERROR] corosync package not installed! Run setup.sh firstly..."
+    echo -e "\a${warn}[ERROR] ${txtrst}corosync package not installed! Run setup.sh firstly..."
     echo
     exit 1;
 fi
 
 echo "Configuring corosync..."
-cp -f /etc/corosync/corosync.conf.example /etc/corosync/corosync.conf
-sed -i.bak "s/.*mcastaddr:.*/mcastaddr:\ $farm_mcast/g" /etc/corosync/corosync.conf
-sed -i.bak "s/.*mcastport:.*/mcastport:\ $farm_port/g" /etc/corosync/corosync.conf
-sed -i.bak "s/.*bindnetaddr:.*/bindnetaddr:\ $farm_addr/g" /etc/corosync/corosync.conf
-
-cat << EOT >> /etc/corosync/service.d/pcmk
+cp -f $COROSYNC_CONF.example $COROSYNC_CONF
+sed -i.bak "s/.*mcastaddr:.*/mcastaddr:\ $FARM_MCAST/g" $COROSYNC_CONF
+sed -i.bak "s/.*mcastport:.*/mcastport:\ $FARM_PORT/g" $COROSYNC_CONF
+sed -i.bak "s/.*bindnetaddr:.*/bindnetaddr:\ $FARM_SERV_ADDR/g" $COROSYNC_CONF
+cat << EOT > /etc/corosync/service.d/pcmk
 service {
         # Load the Pacemaker Cluster Resource Manager
         name: pacemaker
@@ -37,11 +49,60 @@ service {
 }
 EOT
 echo
-echo "[ OK ]"
+echo "${pass}[ OK ]"
 echo
 
 /etc/init.d/corosync start
 
+echo "Checking status of corosync..."
+echo
+if [ 4 -gte "`grep -c "[TOTEM ]" /var/log/messages`" ]; then
+    echo "${info}The Corosync Cluster Engine seems to work fine."
+else
+    echo "${warn}An error might occurred! Take a look at your /var/log/messages log..."
+fi
+echo
+
+/etc/init.d/pacemaker start
+
+echo
+echo "Configuring pacemaker services..."
+echo
+
+# Disables node isolating for failed cluster
+crm configure property stonith-enabled=false
+# Ignores clusters without quorum
+crm configure property no-quorum-policy=ignore
+# Checks the configuration
+crm_verify -L
+# Registers heartbeat IP state resources
+crm configure primitive ClusterIP ocf:heartbeat:IPaddr2 params ip=$FARM_ADDR cidr_netmask=24 op monitor interval=5s
+# Checks where the IP cluster is running on
+#crm resource status ClusterIP
+# Registers heartbeat Apache resource
+crm configure primitive WebApp ocf:heartbeat:apache params configfile=/etc/httpd/conf/httpd.conf op monitor interval=10s
+# Forces the Apache and IP resources to run on the same host
+crm configure colocation ulr-acg-project inf: WebApp ClusterIP
+# Forces cluster to start IP resource BEFORE Apache resource when heads
+crm configure order apache-after-ip inf: ClusterIP WebApp
+# Registers DRBD heartbeat resource
+crm cib new drbd
+crm cib drbd configure primitive VarData ocf:linbit:drbd params drbd_resource=ulracg op monitor interval=30s
+# Configures DRBD master/slave
+crm cib drbd configure ms VarDataClone VarData meta master-max=1 master-node-max=1 clone-max=2 clone-node-max=1 notify=true
+crm cib commit drbd
+# Registers FS heartbeat resource
+crm cib new fs
+crm cib fs configure primitive FS ocf:heartbeat:Filesystem params device="/dev/drbd/by-res/ulracg" directory="/var" fstype="ext4"
+# Forces the FS resource runing on the master DRBD clone
+crm cib fs configure colocation fs_on_drbd inf: FS VarDataClone:Master
+# Forces the FS to run after the DRBD has started
+crm cib fs configure order FS-after-VarData inf: VarDataClone:promote FS:start
+# Forces the Apache resource to depend on the FS
+crm cib fs configure colocation WebApp-with-FS inf: WebApp FS
+# Forces the Apache resource to start after the FS resource
+crm cib fs configure order WebApp-after-FS inf: FS WebApp
+crm cib commit fs
 echo
 echo
 
