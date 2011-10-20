a223bd8b30ed3528e6d027a7d96636431b18bea1
laurent.le.moine17@gmail.com <laurent.le.moine17@gmail.com@663ef947-ee91-8a61-d4fd-d8c2e4fbfccd>
2011-10-20 14:33:06 +0000

    Ajout bind.sh et fichiers de confs bind

--- trunk/confs/mysql/produit.sql
+++ trunk/confs/mysql/produit.sql
@@ -1,22 +1,21 @@
 
-CREATE USER "tpuser";
-SET PASSWORD FOR "tpuser" = password("tpuser");
+CREATE USER 'tpuser' IDENTIFIED BY 'tpuser';
 
 CREATE DATABASE projet_hd;
 
 USE projet_hd;
 
-CREATE TABLE produit 
+CREATE TABLE product 
 (
 	id SMALLINT NOT NULL AUTO_INCREMENT,
-	nom VARCHAR(30) NOT NULL,
-	prix SMALLINT NOT NULL,
-	quantite SMALLINT NOT NULL,
+	name VARCHAR(30) NOT NULL,
+	price SMALLINT NOT NULL,
+	quantity SMALLINT NOT NULL,
 	PRIMARY KEY (id)
 );
 
-INSERT INTO produit(nom, prix, quantite) VALUES('A Games of Thrones', 10.5, 9);
-INSERT INTO produit(nom, prix, quantite) VALUES('A Clash of Kings', 10.5, 2);
-INSERT INTO produit(nom, prix, quantite) VALUES('A Storm of Swords', 10.5, 12);
-INSERT INTO produit(nom, prix, quantite) VALUES('A Feast for Crows', 10.5, 19);
-INSERT INTO produit(nom, prix, quantite) VALUES('A Dance with a Dragon', 10.5, 3);
+INSERT INTO product(name, price, quantity) VALUES('A Games of Thrones', 10.5, 9);
+INSERT INTO product(name, price, quantity) VALUES('A Clash of Kings', 10.5, 2);
+INSERT INTO product(name, price, quantity) VALUES('A Storm of Swords', 10.5, 12);
+INSERT INTO product(name, price, quantity) VALUES('A Feast for Crows', 10.5, 19);
+INSERT INTO product(name, price, quantity) VALUES('A Dance with a Dragon', 10.5, 3);
