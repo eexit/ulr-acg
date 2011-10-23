57cc8b1c57e5bae28cbecea3b66aa2757b955f01
laurent.le.moine17@gmail.com <laurent.le.moine17@gmail.com@663ef947-ee91-8a61-d4fd-d8c2e4fbfccd>
2011-10-23 16:53:53 +0000

    Ajout de users.sql, my.cnf et mysql.sh commentés

---
+++ confs/mysql/users.sql
@@ -0,0 +1,8 @@
+CREATE USER 'tpuser' IDENTIFIED BY 'tpuser';
+
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
+
+CREATE DATABASE projet_hd;
+
+GRANT SELECT PRIVILEGES ON projet_hd.* TP 'tpuser'@'10.192.10.%';
+
--- scripts/mysql.sh
+++ scripts/mysql.sh
@@ -15,6 +15,7 @@ if [ "`whoami`" != "root" ]; then
     exit 1;
 fi
 SQL_SCRIPT=`pwd`/../confs/mysql/produit.sql
+USER_SQL_SCRIPT=`pwd`/../confs/mysql/users.sql
 MYSQL_CONF=`pwd`/../confs/mysql/my.cnf
 echo
 echo
@@ -26,19 +27,28 @@ echo
 echo "Script developped by Joris Berthelot and Laurent Le Moine Copyright (c) 2011"
 echo
 echo
+# On arrete mysqld avant de le configurer
 /etc/init.d/mysqld stop
 echo "Configuring mysqld...."
 echo
+# On remplace l'ancien fichier de configuration par le nouveau
 cp -f -v $MYSQL_CONF /etc/my.cnf
+# On donne l'accès à mysqld au répertoire nécessaires
 chmod -R 755 /var/log
 chown -R mysql:mysql /var/cluster/mysql
 echo "[   ${bldblu}OK${txtrst}   ]"
 echo
+# On démarre mysqld
 /etc/init.d/mysqld start
 echo
 read -p "Please choose a root password : " ROOT_PASSWD
 mysqladmin password $ROOT_PASSWD
 echo
+echo "Creating the users...."
+# On lance un script qui crée et donne des privilèges aux différents utilisateurs
+mysqld -f --password=$ROOT_PASSWD < $USER_SQL_SCRIPT
+echo
+# Si la machine est le noeud principal, on éxécute un script qui crée la base projet_hd et la table product, puis la peuple 
 read -p "Are you the primary node cluster (data reference)? (y/n) " -n 1 ANS
 echo
 if [ "y" == $ANS ]; then
