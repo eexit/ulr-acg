059eb0c0628643ae2d15ae318337627cd4d077cf
Joris Berthelot <admin+github@eexit.net>
2011-10-26 20:26:03 +0000

    Rapport 1.01

--- confs/mysql/produit.sql
+++ confs/mysql/produit.sql
@@ -1,17 +1,17 @@
+CONNECT projet_hd;
 
-USE projet_hd;
-
-CREATE TABLE product 
-(
-	id SMALLINT NOT NULL AUTO_INCREMENT,
-	name VARCHAR(30) NOT NULL,
-	price SMALLINT NOT NULL,
-	quantity SMALLINT NOT NULL,
-	PRIMARY KEY (id)
+CREATE TABLE product (
+    id SMALLINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    price SMALLINT NOT NULL,
+    quantity SMALLINT NOT NULL,
+    PRIMARY KEY (id)
 );
 
-INSERT INTO product(name, price, quantity) VALUES('A Games of Thrones', 11, 9);
-INSERT INTO product(name, price, quantity) VALUES('A Clash of Kings', 11, 2);
-INSERT INTO product(name, price, quantity) VALUES('A Storm of Swords', 11, 12);
-INSERT INTO product(name, price, quantity) VALUES('A Feast for Crows', 11, 19);
-INSERT INTO product(name, price, quantity) VALUES('A Dance with a Dragon', 11, 3);
+INSERT INTO product(name, price, quantity) VALUES(
+    ('A Games of Thrones', 11, 9),
+    ('A Clash of Kings', 11, 2),
+    ('A Storm of Swords', 11, 12),
+    ('A Feast for Crows', 11, 19),
+    ('A Dance with a Dragon', 11, 3)
+);
